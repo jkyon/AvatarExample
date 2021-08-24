4732ef7298272fc5f588a7689a11ad847ad8b734
jkyon <orjuela.juanmanuel@gmail.com>
2021-08-23 20:36:22 -0500

    Add demo project

---
+++ Assets/Scripts/OutfitChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutfitChanger : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer bodyPart;
+
+    [SerializeField]
+    private List<Sprite> options = new List<Sprite>();
+
+    private int optionSelected = 0;
+
+    public void NextOption()
+    {
+        optionSelected++;
+
+        if (this.optionSelected >= this.options.Count)
+        {
+            this.optionSelected = 0;
+        }
+
+        bodyPart.sprite = options[this.optionSelected];
+    }
+
+    public void PreviousOption()
+    {
+        optionSelected--;
+
+        if (this.optionSelected <= this.options.Count)
+        {
+            this.optionSelected = this.options.Count - 1;
+        }
+
+        bodyPart.sprite = options[this.optionSelected];
+    }
+}
